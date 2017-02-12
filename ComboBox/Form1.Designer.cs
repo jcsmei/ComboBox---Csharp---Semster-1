f4fd6926bd33dfdd2fd2b07c8299ceca5405baf7
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:05:57 -0500

    Add project files.

---
+++ ComboBox/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ComboBox
+{
+    partial class SelectionSwitchStatement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCountries = new System.Windows.Forms.ComboBox();
+            this.lblCountries = new System.Windows.Forms.Label();
+            this.btnMultiSwitch = new System.Windows.Forms.Button();
+            this.btnPrize = new System.Windows.Forms.Button();
+            this.btnVoting = new System.Windows.Forms.Button();
+            this.btnDriverSwitch = new System.Windows.Forms.Button();
+            this.btnEat = new System.Windows.Forms.Button();
+            this.btnVendingSwitch = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboCountries
+            // 
+            this.cboCountries.FormattingEnabled = true;
+            this.cboCountries.Items.AddRange(new object[] {
+            "The United States",
+            "Canada",
+            "Mexico"});
+            this.cboCountries.Location = new System.Drawing.Point(256, 292);
+            this.cboCountries.Name = "cboCountries";
+            this.cboCountries.Size = new System.Drawing.Size(343, 21);
+            this.cboCountries.TabIndex = 0;
+            this.cboCountries.SelectedIndexChanged += new System.EventHandler(this.cboCountries_SelectedIndexChanged);
+            // 
+            // lblCountries
+            // 
+            this.lblCountries.AutoSize = true;
+            this.lblCountries.Location = new System.Drawing.Point(50, 295);
+            this.lblCountries.Name = "lblCountries";
+            this.lblCountries.Size = new System.Drawing.Size(137, 13);
+            this.lblCountries.TabIndex = 1;
+            this.lblCountries.Text = "Countries and their capitals:";
+            // 
+            // btnMultiSwitch
+            // 
+            this.btnMultiSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiSwitch.Location = new System.Drawing.Point(53, 152);
+            this.btnMultiSwitch.Name = "btnMultiSwitch";
+            this.btnMultiSwitch.Size = new System.Drawing.Size(140, 55);
+            this.btnMultiSwitch.TabIndex = 2;
+            this.btnMultiSwitch.Text = "Multiple Alternative \r\nSwitch Statement";
+            this.btnMultiSwitch.UseVisualStyleBackColor = true;
+            this.btnMultiSwitch.Click += new System.EventHandler(this.btnMultiSwitch_Click);
+            // 
+            // btnPrize
+            // 
+            this.btnPrize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrize.Location = new System.Drawing.Point(459, 213);
+            this.btnPrize.Name = "btnPrize";
+            this.btnPrize.Size = new System.Drawing.Size(140, 55);
+            this.btnPrize.TabIndex = 3;
+            this.btnPrize.Text = "Prize";
+            this.btnPrize.UseVisualStyleBackColor = true;
+            this.btnPrize.Click += new System.EventHandler(this.btnPrize_Click);
+            // 
+            // btnVoting
+            // 
+            this.btnVoting.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoting.Location = new System.Drawing.Point(459, 152);
+            this.btnVoting.Name = "btnVoting";
+            this.btnVoting.Size = new System.Drawing.Size(140, 55);
+            this.btnVoting.TabIndex = 4;
+            this.btnVoting.Text = "Voting Eligibility";
+            this.btnVoting.UseVisualStyleBackColor = true;
+            this.btnVoting.Click += new System.EventHandler(this.btnVoting_Click);
+            // 
+            // btnDriverSwitch
+            // 
+            this.btnDriverSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDriverSwitch.Location = new System.Drawing.Point(265, 213);
+            this.btnDriverSwitch.Name = "btnDriverSwitch";
+            this.btnDriverSwitch.Size = new System.Drawing.Size(140, 55);
+            this.btnDriverSwitch.TabIndex = 5;
+            this.btnDriverSwitch.Text = "Driver License\r\nSwitch Statement";
+            this.btnDriverSwitch.UseVisualStyleBackColor = true;
+            this.btnDriverSwitch.Click += new System.EventHandler(this.btnDriverSwitch_Click);
+            // 
+            // btnEat
+            // 
+            this.btnEat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEat.Location = new System.Drawing.Point(265, 152);
+            this.btnEat.Name = "btnEat";
+            this.btnEat.Size = new System.Drawing.Size(140, 55);
+            this.btnEat.TabIndex = 6;
+            this.btnEat.Text = "Where to Eat";
+            this.btnEat.UseVisualStyleBackColor = true;
+            this.btnEat.Click += new System.EventHandler(this.btnEat_Click);
+            // 
+            // btnVendingSwitch
+            // 
+            this.btnVendingSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVendingSwitch.Location = new System.Drawing.Point(53, 213);
+            this.btnVendingSwitch.Name = "btnVendingSwitch";
+            this.btnVendingSwitch.Size = new System.Drawing.Size(140, 55);
+            this.btnVendingSwitch.TabIndex = 7;
+            this.btnVendingSwitch.Text = "Vending Machine\r\nSwith Statement";
+            this.btnVendingSwitch.UseVisualStyleBackColor = true;
+            this.btnVendingSwitch.Click += new System.EventHandler(this.btnVendingSwitch_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(32, 12);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(567, 109);
+            this.txtDisplay.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(53, 334);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // SelectionSwitchStatement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 372);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnVendingSwitch);
+            this.Controls.Add(this.btnEat);
+            this.Controls.Add(this.btnDriverSwitch);
+            this.Controls.Add(this.btnVoting);
+            this.Controls.Add(this.btnPrize);
+            this.Controls.Add(this.btnMultiSwitch);
+            this.Controls.Add(this.lblCountries);
+            this.Controls.Add(this.cboCountries);
+            this.Name = "SelectionSwitchStatement";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCountries;
+        private System.Windows.Forms.Label lblCountries;
+        private System.Windows.Forms.Button btnMultiSwitch;
+        private System.Windows.Forms.Button btnPrize;
+        private System.Windows.Forms.Button btnVoting;
+        private System.Windows.Forms.Button btnDriverSwitch;
+        private System.Windows.Forms.Button btnEat;
+        private System.Windows.Forms.Button btnVendingSwitch;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ComboBox/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ComboBox
+{
+    public partial class SelectionSwitchStatement : Form
+    {
+        public SelectionSwitchStatement()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultiSwitch_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox("Type G for green, O for Orange, V for violet, R for red," + 
+                "Y for Yellow, other letters for white.");
+            switch (strUser.ToUpper())
+            {
+                case "G":
+                txtDisplay.BackColor = Color.Green;
+                break;
+                case "O":
+                    txtDisplay.BackColor = Color.Orange;
+                    break;
+                case "V":
+                    txtDisplay.BackColor = Color.Violet;
+                    break;
+                case "R":
+                    txtDisplay.BackColor = Color.Red;
+                    break;
+                case "Y":
+                    txtDisplay.BackColor = Color.Yellow;
+                    break;
+                default:
+                    txtDisplay.BackColor = Color.White;
+                    break;
+            }
+        }
+
+        private void btnVendingSwitch_Click(object sender, EventArgs e)
+        {
+            string strAnswer = Interaction.InputBox(@"Type 10 for Pepsi; 
+15 for Coca Cola; 
+18 for Snapple; 
+22 for Ginger Ale; 
+30 for Gatorade");
+            switch (strAnswer)
+            {
+                case "10":
+                    txtDisplay.Text = "Here is a Pepsi.";
+                    break;
+                case "15":
+                    txtDisplay.Text = "Here is a Coco Cola.";
+                    break;
+                case "18":
+                    txtDisplay.Text = "Here is a Snapple.";
+                    break;
+                case "22":
+                    txtDisplay.Text = "Here is a Ginger Ale.";
+                    break;
+                case "30":
+                    txtDisplay.Text = "Here is a Gatorade.";
+                    break;
+                default:
+                    txtDisplay.Text = strAnswer + " Invalid Entry! Try Again.";
+                    break;
+            }
+
+        }
+
+        private void btnEat_Click(object sender, EventArgs e)
+        {
+            string Food  =  Interaction.InputBox(@"What kind of food do you like?
+1 for Jamaican food;
+2 for Chinese food;
+3 for Mexican food; 
+4 for American food.");
+            switch (Food)
+            {
+                case "1":
+                    txtDisplay.Text = "Go to Golden Crust for Jaimaican food.";
+                    break;
+                case "2":
+                    txtDisplay.Text = "Go to Simply Ming for Chinese food.";
+                    break;
+                case "3":
+                    txtDisplay.Text = "Go to Yummy Taco for Mexican food.";
+                    break;
+                case "4":
+                    txtDisplay.Text = "Go to McDonald for American food.";
+                    break;
+                default:
+                    txtDisplay.Text = "Sorry, I don't understand your answer. Try Again.";
+                    break;
+            }
+        }
+
+        private void btnVoting_Click(object sender, EventArgs e)
+        {
+            string strAge = Interaction.InputBox("How old are you?");
+            byte bytAge = 0;
+            if (byte.TryParse(strAge, out bytAge) == false)
+            {
+                txtDisplay.Text = "Invalid age. Try again.";
+                return;
+            }
+            if (bytAge >= 18)
+            {
+                string voterregistration = Interaction.InputBox("Have you registered fo election?" + " Y for Yes and other letters fro No");
+                if (voterregistration.ToUpper() == "Y")
+                {
+                    txtDisplay.Text = "You may vote.";
+                }
+                else
+                {
+                    txtDisplay.Text = "You have to register before you can vote.";
+                }
+             
+            }
+            else
+            {
+                txtDisplay.Text = "You are too young to vote now.";
+            }
+        }
+
+        private void btnDriverSwitch_Click(object sender, EventArgs e)
+        {
+            byte bytAge = 0;
+            string strAge = Interaction.InputBox("How old are you?)" +
+                Environment.NewLine + "If you are 18 tomorrow, you should answer 17.");
+            if (byte.TryParse(strAge, out bytAge) == false)
+            {
+                txtDisplay.Text = "Please enter a reasonable integer number for age." +
+                    strAge + " is not correct. Try Again.";
+                return;
+            }
+            switch (bytAge)
+            {
+                case 16:
+                case 17:
+                    txtDisplay.Text = "You may apply for a driving permit, and drive under supervision.";
+                    break;
+                default:
+                    if (bytAge >= 18)
+                    {
+                        txtDisplay.Text = "You may apply for a driver license.";
+                    }
+                    else if (bytAge < 18)
+                    {
+                        txtDisplay.Text = "You are too young to drive.";
+                    }
+                    break;
+            }
+        }
+
+        private void btnPrize_Click(object sender, EventArgs e)
+        {
+            string strNumber = Interaction.InputBox("What number do you have?");
+            int intNumber = 0;
+            if (int.TryParse(strNumber, out intNumber))
+            {
+                switch (intNumber)
+                {
+                    case 96:
+                        txtDisplay.Text = "You won a first prize: a 6 inch smartphone!";
+                        break;
+                    case 23:
+                    case 39:
+                    case 87:
+                        txtDisplay.Text = "You won a second prize: a 9 inch tablet.";
+                        break;
+                    case 60:
+                    case 61:
+                    case 62:
+                    case 63:
+                    case 64:
+                    case 65:
+                    case 66:
+                    case 67:
+                    case 68:
+                    case 70:
+                        txtDisplay.Text = "You have won a third prize: a 60GB flash drive.";
+                        break;
+                    default:
+                        if (intNumber >= 100)
+                        {
+                            txtDisplay.Text = "You won a mouse pad.";
+                        }
+                        else
+                        {
+                            txtDisplay.Text = "Sorry, no prize for that number.";
+                        }
+                        break;
+                 
+
+                }
+            }
+        }
+
+        private void cboCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string country = cboCountries.Text;
+            switch (country)
+            {
+                case "The United States":
+                    txtDisplay.Text = "The capital of" + country + " is Washinton, DC.";
+                    break;
+                case "Canada":
+                    txtDisplay.Text = "The capital of" + country + " is Ottawa.";
+                    break;
+                case "Mexico":
+                    txtDisplay.Text = "The capital of" + country + " is Mexico City.";
+                    break;
+                default:
+                    txtDisplay.Text = "You must select a valid country. Try again.";
+                    break;
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+            txtDisplay.BackColor = Color.White;
+            
+        }
+    }
+}
